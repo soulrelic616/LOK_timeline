0a4360f0be9fe6caccf02bde9ff39f831ff7fadd
Bernardo Baldiviezo <bernardo.bozo@ddbremedy.co.uk>
2024-10-07 12:15:54 +0100

    starts timeline rework

---
+++ main.js
@@ -0,0 +1,358 @@
+//import './style.css';
+
+//https://codepen.io/sbrl/pen/zNdqdd (smoke effect threejs)
+
+import "./styles/normalize.css";
+import "./styles/styles.less";
+
+import $ from 'jquery';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+//import { particlesCursor } from 'threejs-toys'
+
+//Scrollmagic
+import ScrollMagic from 'scrollmagic';
+
+//test json data for references list
+var json;
+console.log('Hello');
+function loadJSON(reference) {
+    console.log("loaded");
+    $.getJSON("./json/timeline.json")
+        .done(function (data) {
+            json = data;
+            console.log(data);
+            createTimeline(json)
+            return json;
+        })
+        .fail(function (jqxhr, textStatus, error) {
+            var err = textStatus + ", " + error;
+            console.log("Request Failed: " + err);
+        });
+}
+
+loadJSON();
+
+function createTimeline(json) {
+    // Assuming you have a <ul> element with the ID 'milestones' in your HTML
+    const $milestones = $("ul#timeline");
+
+    // Clear existing list items (optional)
+    $milestones.empty();
+
+    // Iterate over each item in the JSON array
+    json.forEach(item => {
+        // Create an <li> element
+        const $li = $("<li></li>").addClass(item.class); // Assign the classes
+
+        // Check if paradox is true
+        if (item.paradox) {
+            // If true, wrap the content in an <a> tag
+            const $link = $("<a></a>").html(item.content);
+            $li.append($link);
+        } else if(item.title){
+            const $title = $("<h2></h2>").html(item.content);
+            $li.append($title);
+        } 
+        else if(item.subtitle){
+            const $subtitle = $("<h3></h3>").html(item.content);
+            $li.append($subtitle);
+        } 
+        else {
+            // If false, just add the content
+            $li.html(item.content);
+        }
+
+        // Append the <li> to the <ul>
+        $milestones.append($li);
+    });
+}
+
+// class Smoke {
+
+//     constructor(options) {
+//       const defaults = {
+//         width: window.innerWidth,
+//         height: window.innerHeight
+//       };
+  
+//       Object.assign(this, options, defaults);
+//       this.onResize = this.onResize.bind(this);
+  
+//       this.addEventListeners();
+//       this.init();
+//     }
+  
+//     init() {
+//       const { width, height } = this;
+  
+//       this.clock = new THREE.Clock();
+  
+//       const renderer = this.renderer = new THREE.WebGLRenderer();
+  
+//       renderer.setSize(width, height);
+  
+//       this.scene = new THREE.Scene();
+  
+//       const meshGeometry = new THREE.BoxGeometry(200, 200, 200);
+//       const meshMaterial = new THREE.MeshLambertMaterial({
+//         color: 0xaa6666,
+//         wireframe: false
+//       });
+//       this.mesh = new THREE.Mesh(meshGeometry, meshMaterial);
+  
+//       this.cubeSineDriver = 0;
+  
+//       this.addCamera();
+//       this.addLights();
+//       this.addParticles();
+//       this.addBackground();
+  
+//       document.body.appendChild(renderer.domElement);
+//     }
+  
+//     evolveSmoke(delta) {
+//       const { smokeParticles } = this;
+  
+//       let smokeParticlesLength = smokeParticles.length;
+  
+//       while(smokeParticlesLength--) {
+//         smokeParticles[smokeParticlesLength].rotation.z += delta * 0.2;
+//       }
+//     }
+  
+//     addLights() {
+//       const { scene } = this;
+//       const light = new THREE.DirectionalLight(0xffffff, 0.75);
+  
+//       light.position.set(-1, 0, 1);
+//       scene.add(light);
+//     }
+  
+//     addCamera() {
+//       const { scene } = this;
+//       const camera = this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 1, 10000);
+  
+//       camera.position.z = 1000;
+//       scene.add(camera);
+//     }
+  
+//     addParticles() {
+//       const { scene } = this;
+//       const textureLoader = new THREE.TextureLoader();
+//       const smokeParticles = this.smokeParticles = [];
+  
+//       textureLoader.load('https://rawgit.com/marcobiedermann/playground/master/three.js/smoke-particles/dist/assets/images/clouds.png', texture => {
+//         const smokeMaterial = new THREE.MeshLambertMaterial({
+//           color: 0xffffff,
+//           map: texture,
+//           transparent: true
+//         });
+//         smokeMaterial.map.minFilter = THREE.LinearFilter;
+//         const smokeGeometry = new THREE.PlaneGeometry(300, 300);
+  
+//         const smokeMeshes = [];
+//         let limit = 150;
+  
+//         while(limit--) {
+//           smokeMeshes[limit] = new THREE.Mesh(smokeGeometry, smokeMaterial);
+//           smokeMeshes[limit].position.set(Math.random() * 500 - 250, Math.random() * 500 - 250, Math.random() * 1000 - 100);
+//           smokeMeshes[limit].rotation.z = Math.random() * 360;
+//           smokeParticles.push(smokeMeshes[limit]);
+//           scene.add(smokeMeshes[limit]);
+//         }
+//       });
+//     }
+  
+//     addBackground() {
+//       const { scene } = this;
+//       const textureLoader = new THREE.TextureLoader();
+//       const textGeometry = new THREE.PlaneGeometry(600, 320);
+  
+//       textureLoader.load('https://rawgit.com/marcobiedermann/playground/master/three.js/smoke-particles/dist/assets/images/background.jpg', texture => {
+//         const textMaterial = new THREE.MeshLambertMaterial({
+//           blending: THREE.AdditiveBlending,
+//           color: 0xffffff,
+//           map: texture,
+//           opacity: 1,
+//           transparent: true
+//         });
+//         textMaterial.map.minFilter = THREE.LinearFilter;
+//         const text = new THREE.Mesh(textGeometry, textMaterial);
+  
+//         text.position.z = 800;
+//         scene.add(text);
+//       });
+//     }
+  
+//     render() {
+//       const { mesh } = this;
+//       let { cubeSineDriver } = this;
+  
+//       cubeSineDriver += 0.01;
+  
+//       mesh.rotation.x += 0.005;
+//       mesh.rotation.y += 0.01;
+//       mesh.position.z = 100 + Math.sin(cubeSineDriver) * 500;
+  
+//       this.renderer.render(this.scene, this.camera);
+//     }
+  
+//     update() {
+//       this.evolveSmoke(this.clock.getDelta());
+//       this.render();
+  
+//       requestAnimationFrame(this.update.bind(this));
+//     }
+  
+//     onResize() {
+//       const { camera } = this;
+  
+//       const windowWidth  = window.innerWidth;
+//       const windowHeight = window.innerHeight;
+  
+//       camera.aspect = windowWidth / windowHeight;
+//       camera.updateProjectionMatrix();
+  
+//       this.renderer.setSize(windowWidth, windowHeight);
+//     }
+  
+//     addEventListeners() {
+//       window.addEventListener('resize', this.onResize);
+//     }
+  
+//   }
+  
+//   /* app.js */
+  
+//   const smoke = new Smoke();
+  
+//   smoke.update();
+
+
+// Setup
+/* const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const threerenderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+    alpha: true // Enable transparency
+});
+
+threerenderer.setPixelRatio(window.devicePixelRatio);
+threerenderer.setSize(window.innerWidth, window.innerHeight);
+threerenderer.setClearColor(0x000000, 0); // Set clear color to transparent
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+// Loading Manager
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onLoad = function() {
+    // Everything is loaded, start rendering
+    document.body.onscroll = moveCamera;
+    moveCamera();
+    animate();
+};
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff); // Soft white ambient light
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(10, 10, 10); // Position the light
+scene.add(directionalLight);
+
+const pointLight = new THREE.PointLight(0xff0000, 1, 100); // Red point light
+pointLight.position.set(0, 0, 10); // Position the light
+scene.add(pointLight);
+
+// Helpers
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper)
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+
+// Avatar
+const loader = new GLTFLoader(loadingManager);
+let classicMac;
+
+function loadMac(){
+    loader.load('/models/mac.gltf', function(gltf) {
+        classicMac = gltf.scene;
+        scene.add(classicMac);
+    
+        // Set the position and scale of classicMac after it's loaded
+        classicMac.position.z = -5;
+        classicMac.position.x = 2;
+        classicMac.scale.set(10, 10, 10); // Scaling the model
+        
+        // Make classicMac face the camera
+        classicMac.rotation.y = Math.PI; // 180 degrees
+    
+        // List all meshes
+        classicMac.traverse((child) => {
+            if (child.isMesh) {
+                console.log(`Mesh name: ${child.name}`);
+    
+                // Make the material more reflective
+                child.material.metalness = 0.2;
+                child.material.roughness = 0.5;
+                //child.material.envMap = envMap; // Apply environment map for reflections
+                child.material.needsUpdate = true;
+            }
+        });
+    
+        // Add the scroll event listener after classicMac is loaded
+        document.body.onscroll = moveCamera;
+        moveCamera();
+    
+        // Call the function to initialize the video texture
+        //initVideoTexture();
+    }, undefined, function(error) {
+        console.error(error);
+    });
+}
+loadMac();
+
+// Scroll Animation
+let previousScrollTop = 0; // Track the previous scroll position
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+
+    // Rotate the moon
+    //moon.rotation.y += 0.030;
+
+    // Calculate the change in scroll position
+    const scrollDelta = previousScrollTop - t;
+
+    // Rotate classicMac if it's defined
+    if (classicMac) {
+        classicMac.rotation.z += scrollDelta * 0.001; // Adjust the multiplier as needed for the desired rotation speed
+        classicMac.rotation.y += scrollDelta * 0.001; // Adjust the multiplier as needed for the desired rotation speed
+    }
+
+    // Update the camera position and rotation based on scroll
+    camera.position.z = t * -0.01;
+    camera.position.x = t * -0.0002;
+    camera.rotation.y = t * -0.0002;
+
+    // Update the previous scroll position
+    previousScrollTop = t;
+}
+
+// Animation Loop
+function animate() {
+    requestAnimationFrame(animate);
+
+    //moon.rotation.y += 0.0005;
+
+	//classicMac.rotation.z += 0.01/Math.PI; //
+
+    // controls.update();
+
+    threerenderer.render(scene, camera);
+} */
+
+// No need to call animate() here, it's called in the loading manager's onLoad callback
